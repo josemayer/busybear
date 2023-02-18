da472115a69d2758c3383d63a2fa46150cb76cd2
José Lucas Silva Mayer <josemayer@usp.br>
2023-02-18 19:25:22 -0400

    Update buses API functions to use get_json_data
    Signed-off-by: José Lucas Silva Mayer <josemayer@usp.br>

--- functions/buses.py
+++ functions/buses.py
@@ -5,14 +5,13 @@ import math
 import functions.utils
 
 def authenticate():
-    with open("config/API.json", "r") as api_file:
-        config = json.load(api_file)
-        auth_config = config["auth"]
-        auth_key = os.environ.get("AUTH_SPTRANS_KEY")
-        auth_url = auth_config["api_URL"] + auth_key
+    config = functions.utils.get_json_data("config/API.json")
+    auth_config = config["auth"]
+    auth_key = os.environ.get("AUTH_SPTRANS_KEY")
+    auth_url = auth_config["api_URL"] + auth_key
 
-        auth_response = requests.post(auth_url)
-        return auth_response
+    auth_response = requests.post(auth_url)
+    return auth_response
 
 def get_data(url):
     try:
@@ -32,32 +31,31 @@ def get_data(url):
     return data
 
 def retrieve_buses(way):
-    with open("config/API.json", "r") as api_file:
-        config = json.load(api_file)
-        buses_config = config["buses"]
+    config = functions.utils.get_json_data("config/API.json")
+    buses_config = config["buses"]
 
-        if way != 'p3' and way != 'butanta':
-            return []
+    if way != 'p3' and way != 'butanta':
+        return []
 
-        lines = buses_config["ways_lc"][way]
-        buses = []
-        for line in lines:
-            data_URL = buses_config["api_URL"] + str(lines[line])
-            data = get_data(data_URL)
-            
-            vehicles = data["vs"]
+    lines = buses_config["ways_lc"][way]
+    buses = []
+    for line in lines:
+        data_URL = buses_config["api_URL"] + str(lines[line])
+        data = get_data(data_URL)
+        
+        vehicles = data["vs"]
 
-            for vehicle in vehicles:
-                bus = {
-                    "bus_line": line,
-                    "bus_code": vehicle["p"],
-                    "lat": vehicle["py"],
-                    "lng": vehicle["px"]
-                }
+        for vehicle in vehicles:
+            bus = {
+                "bus_line": line,
+                "bus_code": vehicle["p"],
+                "lat": vehicle["py"],
+                "lng": vehicle["px"]
+            }
 
-                buses.append(bus)
+            buses.append(bus)
 
-        return buses
+    return buses
 
 def buses_at_points_with_way(way):
     buses = retrieve_buses(way)
